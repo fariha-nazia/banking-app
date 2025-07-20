40838e1db2c012975d8cce64a92933ec91636ace
Fariha Nazia <farihanazia@Mac.fritz.box>
2025-07-20 20:39:48 +0200

    part 2 done

--- js/home.js
+++ js/home.js
@@ -7,12 +7,38 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
     // to check if the event listener is working or not
     console.log('btn add money clicked.');
 
-    // step 2:fetch the input amount from add-money-input
+    // step 2:fetch the input amount from 
+    // add-money-input for amount
     const addMoneyInput = document.getElementById('add-money-input').value;
     console.log(addMoneyInput);
 
-    // step 3: fetch the pin from add-money-pin-input
+    // step 2: fetch the pin from 
+    // add-money-pin-input for pin
     const addMoneyInputPin = document.getElementById('add-money-pin-input').value;
     console.log(addMoneyInputPin);
 
+    // Step 3: validate the pin - this method is a bad method
+    if(addMoneyInputPin === '1111'){
+        console.log('add money successful');
+
+        // Step 4: fetch the current balance from account-balance
+        const accountBalance = document.getElementById('account-balance').innerText;
+        console.log(accountBalance);
+
+        // step 5: adding new balance with old balance. 
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const accountBalanceNumber = parseFloat(accountBalance);
+        console.log(addMoneyNumber, accountBalanceNumber);
+
+        const newBalance = addMoneyNumber + accountBalanceNumber;
+        console.log(newBalance)
+
+        // step 6: Display the updated balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance
+
+
+    }
+    else{
+        alert('Please input 1111 in Pin');
+    }
 });
